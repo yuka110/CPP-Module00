a68d09eea643ede146aa9a2cea07f3614e60a19d
Yuka Itoh <yitoh@f0r3s15.codam.nl>
2024-01-28 16:39:05 +0100

    ex00 done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,11 +6,12 @@
 /*   By: yitoh <yitoh@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/01/19 17:01:21 by yitoh         #+#    #+#                 */
-/*   Updated: 2024/01/19 17:07:50 by yitoh         ########   odam.nl         */
+/*   Updated: 2024/01/28 16:31:35 by yitoh         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstring>
 
 int main(int ac, char **av)
 {
@@ -18,8 +19,12 @@ int main(int ac, char **av)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
     }
     else {
-        std::cout << av[1] << std::endl;   
-
+        for (int cnt = 1; cnt < ac; cnt++){
+            for (int i = 0; i < (int)strlen(av[cnt]); i++){
+                std::cout << (char)toupper(av[cnt][i]);
+            }
+        }
+        std::cout << std::endl;
     }
     return 0;
 }
