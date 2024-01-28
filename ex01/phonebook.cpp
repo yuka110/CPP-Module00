04296a90d1ce8e30fdb8e328ba6f1a143ce7e675
Yuka Itoh <yitoh@f0r3s15.codam.nl>
2024-01-28 18:00:56 +0100

    basic ex01

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,7 +6,33 @@
 /*   By: yitoh <yitoh@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/01/28 16:37:05 by yitoh         #+#    #+#                 */
-/*   Updated: 2024/01/28 16:37:12 by yitoh         ########   odam.nl         */
+/*   Updated: 2024/01/28 18:00:46 by yitoh         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "phonebook.hpp"
+
+int main(void)
+{
+    std::string action;
+
+    // if (ac != 1)
+    //     return (0);
+    std::cout << "Welcome to PhoneBook. Type ADD, SEARCH, or EXIT for more atcions." << std::endl;
+    std::getline(std::cin, action);
+    if (!action.compare("ADD")){
+        std::cout << "ADD!\n";
+        //add operation
+    }
+    else if (!action.compare("SEARCH")){
+        std::cout << "SEARCH!\n";
+        //search operation
+    }
+    else if (!action.compare("EXIT")){
+        std::cout << "EXIT!\n";
+        //clean data and return 0 operation
+    }
+    else
+        std::cout << "Error. Please type ADD, SEARCH, or EXIT for more atcions." << std::endl;
+    return (0);
+}
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -6,7 +6,29 @@
 /*   By: yitoh <yitoh@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/01/28 16:37:09 by yitoh         #+#    #+#                 */
-/*   Updated: 2024/01/28 16:37:10 by yitoh         ########   odam.nl         */
+/*   Updated: 2024/01/28 17:58:51 by yitoh         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cstring>
+
+
+// class PhoneBook{
+//     // private:
+//     //     /* data */
+//     public:
+//         class Contact;
+// };
+
+// class PhoneBook::Contact{
+//     private:
+//         std::string _nick;
+//         std::string _secret;
+//         int         _phone;
+//     public:
+//         std::string fname;
+//         std::string lname;
+// }
 
